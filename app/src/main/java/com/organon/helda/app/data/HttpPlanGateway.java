8ed8fdd649855c4ac0fbbda21940da32ba88ca05
Francisco Pinho <up201303744@fe.up.pt>
2018-04-13 18:15:19 +0100

    missing changes

--- app/src/main/java/com/organon/helda/app/data/HttpPlanGateway.java
+++ app/src/main/java/com/organon/helda/app/data/HttpPlanGateway.java
@@ -21,7 +21,7 @@ import java.util.Map;
 
 public class HttpPlanGateway implements PlanGateway {
    // private static final String BASE = "https://helda-server.herokuapp.com/";
-    private static final String BASE = "http://192.168.1.65:4567/";
+    private static final String BASE = "http://"+System.getProperty("serverIP")+":4567/";
     private static String GET_PLAN = "plans/%s";
 
     @Override

da982d2658121aa5a6daf76eb5610d6575c7ad91
JoaoIsaias <jl.isaias123@gmail.com>
2018-04-22 18:21:39 +0100

    some stuff

--- app/src/main/java/com/organon/helda/core/Context.java
+++ app/src/main/java/com/organon/helda/core/Context.java
@@ -3,8 +3,10 @@ package com.organon.helda.core;
 import com.organon.helda.core.entities.Anomaly;
 import com.organon.helda.core.gateways.AnomalyGateway;
 import com.organon.helda.core.gateways.PlanGateway;
+import com.organon.helda.core.gateways.TaskTimeTableGateway;
 
 public class Context {
     public PlanGateway planGateway;
     public AnomalyGateway anomalyGateway;
+    public TaskTimeTableGateway taskTimeTableGateway;
 }
---
+++ app/src/main/java/com/organon/helda/core/usecases/registerTaskTime/registerTaskTimeResponseMessage.java
@@ -0,0 +1,9 @@
+package com.organon.helda.core.usecases.registerTaskTime;
+
+import com.organon.helda.core.usecases.ResponseMessage;
+
+import com.organon.helda.core.usecases.ResponseMessage;
+
+public class registerTaskTimeResponseMessage extends ResponseMessage<registerTaskTimeResponseMessage> {
+
+}

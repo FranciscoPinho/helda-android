68fd470ae229fb97a2362ae4b5ffe0582f6318be
João Silva <j.pedro004@gmail.com>
2018-05-14 23:34:24 +0100

    Fix merge error

--- app/src/main/java/com/organon/helda/core/Context.java
+++ app/src/main/java/com/organon/helda/core/Context.java
@@ -3,7 +3,6 @@ package com.organon.helda.core;
 import com.organon.helda.core.gateways.AnomalyGateway;
 import com.organon.helda.core.gateways.DisassemblyGateway;
 import com.organon.helda.core.gateways.PauseGateway;
-import com.organon.helda.core.gateways.PlanGateway;
 import com.organon.helda.core.gateways.TaskTimeTableGateway;
 
 public class Context {
